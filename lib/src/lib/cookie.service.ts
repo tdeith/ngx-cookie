ded09020b5a2d472d9a32af49c1a97e6fdad3917
Taylor Deith <tdeith@gmail.com>
2020-06-10 15:44:18 -0700

    Miscellaneous code cleanup. Fixed deserialization caching.

--- lib/src/lib/cookie.service.ts
+++ lib/src/lib/cookie.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 
 import { CookieOptionsProvider } from './cookie-options-provider';
 import { CookieOptions } from './cookie-options.model';
-import { isBlank, isString, mergeOptions, safeDecodeURIComponent, safeJsonParse } from './utils';
+import { isBlank, isString, mergeOptions, safeDecodeURIComponent, safeJsonParse, isPresent } from './utils';
 
 declare interface Document {
   cookie: string;
@@ -22,6 +22,9 @@ export interface ICookieService {
 @Injectable()
 export class CookieService implements ICookieService {
 
+  private _lastCookies = {};
+  private _lastCookieString = '';
+
   protected options: CookieOptions;
 
   protected get cookieString(): string {
@@ -30,10 +33,6 @@ export class CookieService implements ICookieService {
 
   protected set cookieString(val: string) {
     document.cookie = val;
-  }
-
-  constructor(private _optionsProvider: CookieOptionsProvider) {
-    this.options = this._optionsProvider.options;
   }
 
   /**
@@ -46,7 +45,7 @@ export class CookieService implements ICookieService {
    * @returns Raw cookie value.
    */
   get(key: string): string {
-    return (<any>this._cookieReader())[key];
+    return (<any>this._readCookies())[key];
   }
 
   /**
@@ -72,7 +71,7 @@ export class CookieService implements ICookieService {
    * @returns All cookies
    */
   getAll(): Object {
-    return <any>this._cookieReader();
+    return <any>this._readCookies();
   }
 
   /**
@@ -86,7 +85,7 @@ export class CookieService implements ICookieService {
    * @param options (Optional) Options object.
    */
   put(key: string, value: string, options?: CookieOptions) {
-    this._cookieWriter()(key, value, options);
+    this._writeCookie(key, value, options);
   }
 
   /**
@@ -113,7 +112,7 @@ export class CookieService implements ICookieService {
    * @param options (Optional) Options object.
    */
   remove(key: string, options?: CookieOptions): void {
-    this._cookieWriter()(key, undefined, options);
+    this._writeCookie(key, undefined, options);
   }
 
   /**
@@ -129,38 +128,35 @@ export class CookieService implements ICookieService {
     });
   }
 
-  private _cookieReader(): Object {
-    let lastCookies = {};
-    let lastCookieString = '';
-    let cookieArray: string[], cookie: string, i: number, index: number, name: string;
+  private _readCookies(): Object {
+    let index: number, name: string;
+
     const currentCookieString = this.cookieString;
-    if (currentCookieString !== lastCookieString) {
-      lastCookieString = currentCookieString;
-      cookieArray = lastCookieString.split('; ');
-      lastCookies = {};
-      for (i = 0; i < cookieArray.length; i++) {
-        cookie = cookieArray[i];
-        index = cookie.indexOf('=');
-        if (index > 0) {  // ignore nameless cookies
-          name = safeDecodeURIComponent(cookie.substring(0, index));
-          // the first value that is seen for a cookie is the most
-          // specific one.  values for the same cookie name that
-          // follow are for less specific paths.
-          if (isBlank((<any>lastCookies)[name])) {
-            (<any>lastCookies)[name] = safeDecodeURIComponent(cookie.substring(index + 1));
-          }
-        }
-      }
+    if (currentCookieString === this._lastCookieString) {
+      return this._lastCookies
     }
-    return lastCookies;
+
+    this._lastCookieString = currentCookieString;
+    this._lastCookies = <any>{};
+    for (let cookie of currentCookieString.split('; ')) {
+      index = cookie.indexOf('=');
+      if (index <= 0) {
+        continue;  // ignore nameless cookies
+      }
+      name = safeDecodeURIComponent(cookie.substring(0, index));
+      if (isPresent(this._lastCookies[name])) {
+        // the first value that is seen for a cookie is the most
+        // specific one.  values for the same cookie name that
+        // follow are for less specific paths.
+        continue;
+      }
+      this._lastCookies[name] = safeDecodeURIComponent(cookie.substring(index + 1));
+    }
+    return this._lastCookies;
   }
 
-  private _cookieWriter() {
-    const that = this;
-
-    return function (name: string, value: string, options?: CookieOptions) {
-      that.cookieString = that._buildCookieString(name, value, options);
-    };
+  private _writeCookie(name: string, value: string, options?: CookieOptions) {
+    this.cookieString = this._buildCookieString(name, value, options);
   }
 
   private _buildCookieString(name: string, value: string, options?: CookieOptions): string {
@@ -187,7 +183,7 @@ export class CookieService implements ICookieService {
     // - 4096 bytes per cookie
     const cookieLength = str.length + 1;
     if (cookieLength > 4096) {
-      console.log(`Cookie \'${name}\' possibly not set or overflowed because it was too large (${cookieLength} > 4096 bytes)!`);
+      console.log(`Cookie '${name}' possibly not set or overflowed because it was too large (${cookieLength} > 4096 bytes)!`);
     }
     return str;
   }
